fd9c396dca519af882f7123f89780b589ab668c1
EwenQuim <ewen.quimerch@gmail.com>
2021-12-09 18:57:50 +0100

    app - better routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,38 +5,10 @@ import React, { useEffect, useState } from 'react';
 
 import { Todo } from './types';
 import TodoList from './components/Todo';
+import { Todos } from './components/Todos';
 
 const App = () => {
-  const [input, setInput] = useState("")
-  const [newTodoPrivate, setNewTodoPrivate] = useState(false)
-  const [todos, setTodos] = useState<Todo[]>([])
 
-  const getTodos = () => {
-    fetch('/api/todo')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setTodos(data)
-      })
-  }
-
-
-  const newTodo = async () => {
-    if (input !== "") {
-      setInput("")
-      const response = await fetch('/api/todo/new?title=' + input + '&public=' + !newTodoPrivate)
-      const jsonResponse: Todo = await response.json()
-      console.log(jsonResponse.UUID)
-      // move to another page
-      window.location.href = '/' + jsonResponse.UUID
-      getTodos()
-
-    }
-  };
-
-  useEffect(() => {
-    getTodos()
-  }, [])
 
   if (window.location.pathname !== '/') {
     const path = window.location.pathname.split('/')[1]
@@ -50,46 +22,7 @@ const App = () => {
   }
 
   return (
-    <div className="App">
-
-      <h1>TODO APP 📝</h1>
-      <h2>Create a new todo list !</h2>
-
-      <div className="flex">
-        <input
-          id="todoName"
-          name="todoName"
-          placeholder="Title"
-          className="flex-1 my-2"
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              newTodo();
-            }
-          }}
-        />
-        <button className="m-2 p-2 rounded bg-purple-600" onClick={newTodo}>
-          Create
-        </button>
-      </div>
-      <label htmlFor="newTodoPrivate">🔐 Secret list </label>
-      <input type="checkbox" name="newTodoPrivate" id="newTodoPrivate" checked={newTodoPrivate} onClick={() => setNewTodoPrivate(!newTodoPrivate)} />
-      <br />
-
-
-
-
-      <h2>Public todo lists</h2>
-
-      <ul>
-        {todos.map(todo => {
-          return <li> <a href={todo.UUID}> {todo.Title} </a></li>
-        })}
-      </ul>
-
-    </div>
+    <Todos />
   );
 }
 
